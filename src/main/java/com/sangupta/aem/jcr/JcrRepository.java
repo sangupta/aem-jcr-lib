15496e3a0261f1f87a52ae4d17a9971e2fa560b2
Sandeep Gupta <sandy.pec@gmail.com>
2016-03-21 23:44:22 +0530

    Removing unused imports

--- src/main/java/com/sangupta/aem/jcr/JcrRepository.java
+++ src/main/java/com/sangupta/aem/jcr/JcrRepository.java
@@ -2,16 +2,13 @@ package com.sangupta.aem.jcr;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.lang.StringUtils;
 
